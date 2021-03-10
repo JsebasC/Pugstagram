2554248f799423371ff99161f53104e2836dfc17
JsebasC <jhoansebastian231@hotmail.com>
2021-03-09 23:16:35 -0500

    Props

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,30 @@
 <script>
 	import About from "./components/About.svelte";
+	import Text from "./components/Text.svelte";
+	import Person from "./components/Person.svelte";
+
 	export let saludo;
-	export let Name;
-	export let lastName;	
+	export let Name1;
+	export let lastName1;	
+	export let Age;	
 	let img = "https://i.blogs.es/aa1b9a/luna-100mpx/450_1000.jpg";
+
+
+	const data = {
+		name: "pepitop perez",
+		lastName: "apellido perez",
+		age: 19,
+	}
+
 </script>
 
 <main>
-	<h1>Hello {saludo} {Name} {lastName}! </h1>
+	<h1>Hello {saludo} {Name1} {lastName1}! </h1>
 	<About/>
+	<Text anotherText="Prueba"/>
+	<Text />
+	<Person name={data.name} lastName={data.lastName} age={data.age}/>
+	<Person {...data} />
 	<img src={img} alt="Svelte">
 </main>
 
